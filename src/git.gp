28f5dea31611efc3900e2098b1c13c9fea472076
jonte <>
2015-03-03 00:13:24 +0100

    nicer

--- src/git.gp
+++ src/git.gp
@@ -1,10 +1,19 @@
 set terminal png
 set output "graph.png"
 
+
 binwidth=5
 set boxwidth binwidth
 
 bin(x,width)=width*floor(x/width) + width/2.0
 
+set xlabel "Age in days"
+set ylabel "Number of commits"
 
-plot '<cat' using (bin($1,binwidth)):(1.0) smooth freq with boxes
+set style line 1 lt 2 lw 2 pt 3 ps 0.5
+
+
+set border 3
+
+
+plot '<cat' using (bin($1,binwidth)):(1.0) ls 1 smooth freq with boxes notitle
--- src/main.sh
+++ src/main.sh
@@ -16,8 +16,17 @@ echo ${TAGS[1]}
 
 RECENT_DATE=$(getDateForCommit ${GIT_DIR} ${TAGS[0]})
 
-getCommitDates ${GIT_DIR} ${TAGS[1]} ${TAGS[0]} |  while read -r line; do echo $(dateDiff $line $RECENT_DATE); done | gnuplot ${SCRIPTDIR}/git.gp
+
+function plot() {
+    local gitDir="$1"
+    local fromGitRef="$2"
+    local toGitRef="$3"
+    
+    getCommitDates "$gitDir" "$fromGitRef" "$toGitRef" |  while read -r line; do echo $(dateDiff $line $RECENT_DATE); done | gnuplot ${SCRIPTDIR}/git.gp
+
+}
 
 
  
 
+plot ${GIT_DIR} ${TAGS[1]} ${TAGS[0]}
